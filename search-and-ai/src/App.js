2d32d06056cdd2ab97bbbd9200eeea43fc600cc0
Your Full Name <yourEmailAddress@westpac.com.au>
2024-09-13 12:06:29 +1000

    added npm

--- search-and-ai/src/App.js
+++ search-and-ai/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import {InputBar} from './components/InputBar.jsx'
+import { InputBar } from './components/InputBar.jsx'
 
 function App() {
   return (
